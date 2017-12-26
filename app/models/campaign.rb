0dfa7831affe8f1abe48c831d8548407be74c8b4
Thomas Carney <thomas@thomascarney.org>
2017-12-26 10:54:45 +0100

    Removes unneeded uniqueness requirement on title

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -2,7 +2,7 @@ class Campaign < ApplicationRecord
   belongs_to :user
   has_many :contacts, dependent: :destroy
   accepts_nested_attributes_for :contacts
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true
 
   def render(contact)
     email.gsub(/{{[\w]+}}/) {|var| contact.contact_attributes.find_by(attribute_name: var.scan(/[^({{|}})]/).join).attribute_value }
