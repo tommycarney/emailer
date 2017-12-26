77a0e726f48d607c624465b5717a8e71497bd381
Thomas Carney <thomas@thomascarney.org>
2017-12-26 12:20:06 +0100

    Moves Omniauth test setup to seperate file

---
+++ spec/support/omniauth.rb
@@ -0,0 +1,14 @@
+OmniAuth.config.test_mode = true
+OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
+    :provider => 'google_oauth2',
+    :uid => '123545',
+    :credentials => {
+                     :refresh_token => '12345',
+                     :token         => '54321',
+                     :expires_at    => 1.hour.from_now
+                   },
+
+    :info        =>  {
+                     :email         => 'test@example.com'
+    }
+  })
