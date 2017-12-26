5181a6e0eb41d3eb07af676d14e5f7b976b92ca9
Thomas Carney <thomas@thomascarney.org>
2017-12-26 11:27:17 +0100

    Removes test folder

--- test/models/campaign_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class CampaignTest < ActiveSupport::TestCase
-  test "campaign attributes must not be empty" do
-    campaign = Campaign.new
-    assert campaign.invalid?
-  end
-end
